c2fd457e2ad4ec80627133d8fbf1cf8573b12ce3
aucookson97 <aidanucookson@gmail.com>
2018-10-06 16:17:05 -0400

    Completed rejection Sampling

--- bayesian_network.py
+++ bayesian_network.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import sys
+import random
+import math
 
 class Node():
 
@@ -8,11 +10,34 @@ class Node():
         self.name = name
         self.prob = prob
         self.parents = parents
+        self.value = None
         self.evidence = None
         self.query = None
 
+    def setValue(self, network):
+        if self.value != None:
+            return self.value
+        val = random.random()
+        if (len(self.parents) == 0): # Top Level Node
+            if (val < self.prob[0]):
+                self.value = False
+            else:
+                self.value = True
+        else:
+            index = 0
+            for i in range(len(self.parents)):
+                p = findNodeByName(self.parents[i], network.nodes())
+                if p.setValue(network):
+                    index += math.pow(2, i+1)
+            if val < self.prob[int(index)]:
+                self.value = False
+            else:
+                self.value = True
+        return self.value
+            
+        
     def __str__(self):
-        return("Name: {}\nParents: {}\nProb. Table: {}\nEvidence: {}\nQuery: {}".format(self.name, self.parents, self.prob, self.evidence, self.query))
+        return("Name: {}\nParents: {}\nProb. Table: {}\nEvidence: {}\nQuery: {}\nValue: {}".format(self.name, self.parents, self.prob, self.evidence, self.query, self.value))
 
 def createNetwork(input_file, assignment_file):
     network = nx.DiGraph()
@@ -34,7 +59,6 @@ def createNetwork(input_file, assignment_file):
             table = [float(i) for i in node[2].replace(']', '').split(' ')]
             nodes.append(Node(name, parents, table))
     nodes = assignValue(assignment_file, nodes)
-    printNodes(nodes)
 
     for node in nodes:
         network.add_node(node)
@@ -60,10 +84,41 @@ def assignValue(input_file, node_list):
         if(queries[queryElem] == "f"):
             node_list[queryElem].evidence = False
         if(queries[queryElem] == "-"):
-            node_list[queryElem].query = False
-            node_list[queryElem].evidence = False
+            pass
     return node_list
 
+def rejectionSampling(network, num_samples):
+
+    total_valid_samples = 0
+    num_queries = 0
+    
+    for i in range(num_samples):
+        for node in network.nodes():
+            node.setValue(network)
+        if validEvidence(network):
+            total_valid_samples += 1
+            for node in network.nodes():
+                if node.query:
+                    if node.value:
+                        num_queries += 1
+                    break
+        reset(network)
+
+    probability = num_queries / float(total_valid_samples)
+    return probability
+
+def likelihoodWeighting(network, num_samples):
+    return 1
+
+def validEvidence(network):
+    for node in network.nodes():
+        if (node.evidence != None and node.evidence != node.value):
+            return False
+    return True
+
+def reset(network):
+    for node in network.nodes():
+        node.value = None
 
 def findNodeByName(name, node_list):
     for node in node_list:
@@ -80,8 +135,8 @@ def drawNetwork(network):
     nx.draw(network, pos=pos)#cmap = plt.get_cmap('jet')) #node_color = values)
     plt.show()
 
-def printNodes(node_list):
-    for node in node_list:
+def printNetwork(network):
+    for node in network.nodes():
         print(node)
         print()
 
@@ -92,4 +147,7 @@ if __name__=="__main__":
     input_file = "network_option_a.txt"
     assignment_file = "query1.txt"
     network = createNetwork(input_file, assignment_file)
+    #prob = rejectionSampling(network, 100000)
+    prob = likelihoodWeighting(network, 1000)
+    print ('P(X|e) = {}'.format(prob))
     drawNetwork(network)
