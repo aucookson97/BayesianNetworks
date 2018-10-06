85cb4646ab4e755e33f4cf4f24233427a98865a7
aucookson97 <aidanucookson@gmail.com>
2018-10-06 12:29:21 -0400

    Added Network Graphics

--- bayesian_network.py
+++ bayesian_network.py
@@ -11,7 +11,6 @@ class Node():
 
 
 def createNetwork(input_file):
-
     network = nx.DiGraph()
 
     nodes_raw = []
@@ -25,7 +24,7 @@ def createNetwork(input_file):
         #node = n.split(' ')
         node = n.split('[')
         if len(node) > 1:
-            name = node[0].replace(':', '')
+            name = node[0].replace(':', '').strip()
             parents = node[1].replace(']', '').split(' ')
             parents = [p for p in parents if p != '']
             table = [float(i) for i in node[2].replace(']', '').split(' ')]
@@ -34,6 +33,7 @@ def createNetwork(input_file):
         network.add_node(node)
         for name in node.parents:
             pnode = findNodeByName(name, nodes)
+            #print ('Edge: {}, {}'.format(pnode.name, node.name))
             network.add_edge(pnode, node)
 
     return network
@@ -45,16 +45,13 @@ def findNodeByName(name, node_list):
     return None
 
 def drawNetwork(network):
-    pass
-#pos = {"A": (1, 2),
- #      "B": (2, 3),
-  #     "C": (3, 4)}
-#pos = nx.spring_layout(G)
-
-#values = [val_map.get(node, .25) for node in G.nodes]
-#nx.draw_networkx_labels(G, pos, val_map)
-#nx.draw(G, cmap = plt.get_cmap('jet'), node_color = values)
-#plt.show()
+    pos = nx.spring_layout(network)
+    network_labels = {}
+    for node in network.nodes():
+        network_labels[node] = node.name
+    nx.draw_networkx_labels(network, pos, labels=network_labels)
+    nx.draw(network, pos=pos)#cmap = plt.get_cmap('jet')) #node_color = values)
+    plt.show()
 
 
 if __name__=="__main__":
@@ -62,3 +59,4 @@ if __name__=="__main__":
 #    print (sys.argv)
     input_file = "network_option_a.txt"
     network = createNetwork(input_file)
+    drawNetwork(network)
